3539c4312f9e56af8b73196c3667c4c35d4416f8
Diego Eiras <eiras.lucio@gmail.com>
2020-11-21 15:44:01 -0300

    📦 ADD: Route params support and search user endpoint

--- src/index.js
+++ src/index.js
@@ -6,18 +6,32 @@ const routes = require('./routes');
 const server = http.createServer((request, response) => {
   const parsedUrl = url.parse(request.url, true);
 
-  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
 
-  const route = routes.find(r => 
-    r.endpoint === parsedUrl.pathname && r.method === request.method
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find(route => 
+    route.endpoint === pathname && route.method === request.method
   ); 
 
   if (route) {
     request.query = parsedUrl.query;
+    request.params = { id };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    response.end(`Cannot ${request.method} ${pathname}`);
   }
 });
 
