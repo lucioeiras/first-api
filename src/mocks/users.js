a5a74995337b35a90b702df91f385a8815f0fe74
Diego Eiras <eiras.lucio@gmail.com>
2020-11-21 14:53:23 -0300

    📦 ADD: Users endpoint

---
+++ src/mocks/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 0,
+    name: 'Diego Fernandes',
+  },
+  {
+    id: 1,
+    name: 'Mateus Silva',
+  },
+  {
+    id: 2,
+    name: 'Gabriel Pato',
+  },
+  {
+    id: 3,
+    name: 'Filipe Deschamps',
+  },
+  {
+    id: 4,
+    name: 'Lucas Montano',
+  },
+  {
+    id: 5,
+    name: 'Mayk Brito',
+  },
+]
